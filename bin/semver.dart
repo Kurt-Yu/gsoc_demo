49c7ce11547c87bcc851607b22b0bc32126759e5
xia yu <xiayu19980714@126.com>
2022-02-26 23:25:54 +0800

    retry.dart works correctly

--- bin/semver.dart
+++ bin/semver.dart
@@ -1,48 +1,47 @@
-import 'dart:convert';
 import 'dart:io';
 
-import 'package:args/args.dart';
+import 'package:analyzer/dart/analysis/analysis_context_collection.dart';
+import 'package:analyzer/dart/analysis/results.dart';
+import 'package:analyzer/dart/ast/ast.dart';
+import 'package:analyzer/file_system/physical_file_system.dart';
 
-const lineNumber = 'line-number';
+void main(List<String> args) async {
+  FileSystemEntity entity = Directory.current;
+  if (args.isNotEmpty) {
+    String arg = args.first;
+    entity = FileSystemEntity.isDirectorySync(arg) ? Directory(arg) : File(arg);
+  }
 
-void main(List<String> arguments) {
-  exitCode = 0; // presume success
-  final parser = ArgParser()..addFlag(lineNumber, negatable: false, abbr: 'n');
+  var issueCount = 0;
+  final collection = AnalysisContextCollection(
+      includedPaths: [entity.absolute.path],
+      resourceProvider: PhysicalResourceProvider.INSTANCE);
 
-  ArgResults argResults = parser.parse(arguments);
-  final paths = argResults.rest;
+  // Often one context is returned, but depending on the project structure we
+  // can see multiple contexts.
+  for (final context in collection.contexts) {
+    print("----------------------------------------------");
+    print('Analyzing ${context.contextRoot.root.path} ...');
+    print("----------------------------------------------");
 
-  dcat(paths, showLineNumbers: argResults[lineNumber] as bool);
-}
+    for (final filePath in context.contextRoot.analyzedFiles()) {
+      var session = context.currentSession;
+      var result = await session.getResolvedUnit(filePath);
 
-Future<void> dcat(List<String> paths, {bool showLineNumbers = false}) async {
-  if (paths.isEmpty) {
-    // No files provided as arguments. Read from stdin and print each line.
-    await stdin.pipe(stdout);
-  } else {
-    for (final path in paths) {
-      var lineNumber = 1;
-      final lines = utf8.decoder
-          .bind(File(path).openRead())
-          .transform(const LineSplitter());
-      try {
-        await for (final line in lines) {
-          if (showLineNumbers) {
-            stdout.write('${lineNumber++} ');
+      if (result is ResolvedUnitResult) {
+        CompilationUnit unit = result.unit;
+
+        for (CompilationUnitMember unitMember in unit.declarations) {
+          if (unitMember is ClassDeclaration) {
+            print(unitMember.name.name);
           }
-          stdout.writeln(line);
+          if (unitMember is FunctionDeclaration) {
+            print(unitMember.name.name);
+          }
         }
-      } catch (_) {
-        await _handleError(path);
       }
     }
   }
-}
 
-Future<void> _handleError(String path) async {
-  if (await FileSystemEntity.isDirectory(path)) {
-    stderr.writeln('error: $path is a directory');
-  } else {
-    exitCode = 2;
-  }
+  print('$issueCount issues found.');
 }
